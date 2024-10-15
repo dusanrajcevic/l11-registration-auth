737057bbd6d6afa5d5e280038af9ec345f5c16ff
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2024-10-15 20:43:59 +0200

    Add RegistrationController for showing registration form and storing the registration data.

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Show the registration form.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('register', \App\Http\Controllers\RegistrationController::class)
+    ->only(['index', 'store']);
