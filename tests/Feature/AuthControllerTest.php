46b5c6437f384dbc136f8e3c322db3ab511717ce
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2024-10-17 23:47:23 +0200

    Add tests that verify login post route
    - Check the route authenticates and redirects to home if registration succeeds with username and with email
    - Check the route redirects to login if login is unsuccessful

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -9,6 +9,17 @@ use Tests\TestCase;
 class AuthControllerTest extends TestCase
 {
     use RefreshDatabase;
+    CONST PASSWORD = 'D3a5S#lpK';
+    private User $user;
+
+    public function setup(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'password' => self::PASSWORD,
+        ]);
+    }
 
     public function test_login_page_is_accessible_to_unauthenticated_users_and_contains_proper_fields(): void
     {
@@ -30,5 +41,48 @@ class AuthControllerTest extends TestCase
         $response = $this->actingAs($user)->get(route('login'));
 
         $response->assertRedirect(route('home'));
+    }
+
+    public function test_login_successfully_authenticates_user_with_username_and_redirects_to_home(): void
+    {
+        $this->get(route('login'))->assertStatus(200);
+
+        $data = [
+            'login' => $this->user->username,
+            'password' => self::PASSWORD,
+        ];
+
+        $response = $this->post(route('authenticate'), $data);
+
+        $this->assertAuthenticatedAs($this->user);
+        $response->assertRedirect(route('home'));
+    }
+
+    public function test_login_successfully_authenticates_user_with_email_and_redirects_to_home(): void
+    {
+        $this->get(route('login'))->assertStatus(200);
+
+        $data = [
+            'login' => $this->user->email,
+            'password' => self::PASSWORD,
+        ];
+
+        $response = $this->post(route('authenticate'), $data);
+
+        $this->assertAuthenticatedAs($this->user);
+        $response->assertRedirect(route('home'));
+    }
+
+    public function test_login_unsuccessfully_does_not_authenticate_and_redirects_to_login_page(): void
+    {
+        $this->get(route('login'))->assertStatus(200);
+
+        $data = [
+            'login' => $this->user->email,
+            'password' => 'wrongpassword'
+        ];
+        $response = $this->post(route('authenticate'), $data);
+        $this->assertNull(auth()->user());
+        $response->assertRedirectToRoute('login');
     }
 }
