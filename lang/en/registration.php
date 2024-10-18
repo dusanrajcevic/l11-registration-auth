0721b3e6ab5639aec17e0d59a59a91a32479049e
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2024-10-18 02:07:30 +0200

    Add language strings for the fields and messages.

---
+++ lang/en/registration.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Registration Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |  The following language lines are used for registration fields
+    |  and messages. Feel free to modify these language lines according
+    |  to your application's requirements.
+    |
+    */
+
+    'success' => 'You have successfully registered, and the confirmation link has been sent to your e-mail.',
+    'register' => [
+        'title' => 'Register',
+        'username' => 'Username',
+        'email' => 'E-mail',
+        'password' => [
+            'password' => 'Password',
+            'confirmation' => 'Confirm Password',
+        ],
+        'agreement' => 'Agree',
+        'register' => 'Register',
+    ]
+];
